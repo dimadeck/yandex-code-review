3e03b7e556947e69c2f40d403860339e39636fa0
dimadeck <dimadeck94@gmail.com>
2020-10-09 02:20:20 +0300

    First code review

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # todo CR: Подобный код необходимо убрать, тем более он на главной странице приложения. Хорошим решением будет
+    #  сделать редирект на /api/movies
     return 'worked'
+
 
 @app.route('/api/movies/')
 def movie_list():
     validate = validate_args(request.args)
-
+    # todo CR: Для получения значения словаря рекомендуется использовать 'validate.get('success')'
     if not validate['success']:
         return abort(422)
-
+    # todo CR: В данном случае лучше записать в одну строку - негативно на читаемость это не повлияет
     defaults = {
         'limit': 50,
         'page': 1,
@@ -24,24 +27,32 @@ def movie_list():
         'sort_order': 'asc'
     }
 
+    # todo CR: Бесполезный комментарий
     # Тут уже валидно все
+    # todo CR: Лучше сделать 'for param, value in request.args:', тогда присвоение будет лаконичнее и
+    #  исключит ошибку неправильной передачи ключа: 'defaults[param] = value'
     for param in request.args.keys():
         defaults[param] = request.args.get(param)
 
+    # todo CR: Нагруженный код, допустимо, но может негативно влиять на читаемость
     # Уходит в тело запроса. Если запрос не пустой - мультисерч, если пустой - выдает все фильмы
     body = {
         "query": {
             "multi_match": {
+                # todo CR: значение лучше получать через метод словаря get
                 "query": defaults['search'],
                 "fields": ["title"]
             }
         }
+        # todo CR: значение по-умолчанию в данном случае бесполезно
     } if defaults.get('search', False) else {}
 
+    # todo CR: Можно заполнить одной инструкцией: body['_source'] = {'include': ['id', 'title', 'imdb_rating']}
     body['_source'] = dict()
     body['_source']['include'] = ['id', 'title', 'imdb_rating']
 
     params = {
+        # todo CR: Финальный код нужно чистить от неиспользуемых инструкций
         # '_source': ['id', 'title', 'imdb_rating'],
         'from': int(defaults['limit']) * (int(defaults['page']) - 1),
         'size': defaults['limit'],
@@ -51,7 +62,8 @@ def movie_list():
             }
         ]
     }
-
+    # todo CR: Рекомендуется не использовать "жесткую" конфигурацию при инициализации экземляра.
+    #  Лучше: задать ее в: шапке модуля, конфигурационном модуле, или через входные аргументы.
     es_client = ES.Elasticsearch([{'host': '192.168.11.128', 'port': 9200}], )
     search_res = es_client.search(
         body=body,
@@ -66,19 +78,27 @@ def movie_list():
 
 @app.route('/api/movies/<string:movie_id>')
 def get_movie(movie_id):
+    # todo CR: дублирование данных, еще один аргумент за то, чтобы вынести конфигурацию выше
     es_client = ES.Elasticsearch([{'host': '192.168.11.128', 'port': 9200}], )
 
     if not es_client.ping():
+        # todo CR: Не рекомендуется писать подобные инструкции. Лучше использовать систему логирования(DEBUG)
+        #  с указанием полезных данных
         print('oh(')
 
     search_result = es_client.get(index='movies', id=movie_id, ignore=404)
 
     es_client.close()
 
+    # todo CR: значение лучше получать через метод словаря get
     if search_result['found']:
         return jsonify(search_result['_source'])
+    # todo CR: значение лучше получать через метод словаря get
 
     return abort(404)
 
+
 if __name__ == "__main__":
+    # todo CR: Неплохой код, несколько замечаний, рекомендация к использованию блоков try-except, возврат статусов
+    #  лучше через заранее объявленные переменные (например, status_ok = 200)
     app.run(host='0.0.0.0', port=80)
--- film_loader.py
+++ film_loader.py
@@ -7,14 +7,27 @@ from elasticsearch.helpers import bulk
 
 def extract():
     """
+    # todo CR: Если используются комментарии на русском, то и docstring можно писать на русском для большей
+        информативности и лучшего понимания между коллегами.
+        Также хорошей практикой использования docstring является пояснение с какими данными работает метод,
+        в данном случае: пояснение используемых полей в таблице.
     extract data from sql-db
+    # todo CR: Так как метод возвращает данные, то писать пустой return неправильно.
+        Вариант 1. Пояснить что возвращается (actors, writers, raw_data)
+        Вариант 2. Не писать блок return в данном docstring
     :return:
     """
+    # todo CR: Для решения одной задачи допускается в теле метода использовать жесткие имена файлов,
+    #  но хорошей практикой будет вынести имя базы данных вынести в аргументы: 'def extract(db_name="db.sqlite")' и
+    #  использовать 'sqlite3.connect(db_name)'. Значение по-умолчанию можно не задавать.
     connection = sqlite3.connect("db.sqlite")
     cursor = connection.cursor()
 
+    # todo CR: В комментариях не рекомендуется(часто не допускается) писать подобные мысли. Они не поясняют работу кода,
+    #  если код не устраивает - создать задачу на рефакторинг или не писать ничего.
     # Наверняка это пилится в один sql - запрос, но мне как-то лениво)
 
+    # todo CR: Sql запрос выглядит громоздко. Если есть возможность сделать более простым и читаемым - лучше сделать
     # Получаем все поля для индекса, кроме списка актеров и сценаристов, для них только id
     cursor.execute("""
         select id, imdb_rating, genre, title, plot, director,
@@ -27,13 +40,13 @@ def extract():
                 where movie_id = movies.id
             )
         ),
-        
+
         max(writer, writers)
         from movies
     """)
 
     raw_data = cursor.fetchall()
-
+    # todo CR: Не рекомендуется оставлять закомментированный код в готовом продукте
     # cursor.execute('pragma table_info(movies)')
     # pprint(cursor.fetchall())
 
@@ -44,9 +57,12 @@ def extract():
     return actors, writers, raw_data
 
 
+# todo CR: В данном случае лучше не использовать именование аргументов начиная с '__'. Именование данным способом
+#  применяется для инкапсуляции в классах.
 def transform(__actors, __writers, __raw_data):
     """
-
+    # todo CR: Не желательно оставлять незаполненный docstring. Либо не писать docstring вообще,
+        либо заполнить минимум - Типы входных данных и блок возврата (с указанием типа)
     :param __actors:
     :param __writers:
     :param __raw_data:
@@ -56,7 +72,7 @@ def transform(__actors, __writers, __raw_data):
     for movie_info in __raw_data:
         # Разыменование списка
         movie_id, imdb_rating, genre, title, description, director, raw_actors, raw_writers = movie_info
-
+        # todo CR: В данном случае, условие лучше поменять на 'if raw_writers.startswith('['):'
         if raw_writers[0] == '[':
             parsed = json.loads(raw_writers)
             new_writers = ','.join([writer_row['id'] for writer_row in parsed])
@@ -93,12 +109,13 @@ def transform(__actors, __writers, __raw_data):
 
         for key in document.keys():
             if document[key] == 'N/A':
+                # todo CR: Недопустимый комментарий
                 # print('hehe')
                 document[key] = None
 
         document['actors_names'] = ", ".join([actor["name"] for actor in document['actors'] if actor]) or None
         document['writers_names'] = ", ".join([writer["name"] for writer in document['writers'] if writer]) or None
-
+        # todo CR: инструкции import должны быть в шапке модуля, за исключением особых случаев. Это не тот случай.
         import pprint
         pprint.pprint(document)
 
@@ -106,16 +123,26 @@ def transform(__actors, __writers, __raw_data):
 
     return documents_list
 
+
+# todo CR: В данном случае именование функции может сбить с толку - по факту тут сохранение данных в Elastic.
 def load(acts):
     """
-
+    # todo CR: Незаполненный docstring.
     :param acts:
     :return:
     """
+    # todo CR: Рекомендуется не использовать "жесткую" конфигурацию при инициализации экземляра.
+    #  Лучше: 'def load(acts, elastic_config)', а elastic_config задать в: 1) шапка модуля, 2) конфигурационный модуль,
+    #  3) через входные аргументы.
     es = Elasticsearch([{'host': '192.168.1.252', 'port': 9200}])
     bulk(es, acts)
-
+    # todo CR: Возврат в данном случае не имеет смысла
     return True
 
+
 if __name__ == '__main__':
+    # todo CR: В целом хороший код, удобночитаемый. Отсутствуют блоки try, для отлова ошибок -
+    #  например, отсутствие соединения с Elasticsearch, или данные в таблице будут некорректными.
+    #  Лучше добавить блок try-except в циклы, если где-то ошибка - это не повлияет на выполнение программы в целом,
+    #  основные(правильные) данные будут перенесены в БД.
     load(transform(*extract()))
